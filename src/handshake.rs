2d5934cb126a34dafe72a454009530a1dac40525
kimhyunkang <kimhyunkang@gmail.com>
2016-11-06 10:26:19 -0800

    BufferedIo::nb_read_exact

--- src/handshake.rs
+++ src/handshake.rs
@@ -5,22 +5,22 @@ use std::io::Write;
 use futures::Future;
 use tokio_core::io::{Io, flush, read_until, write_all};
 
-pub enum VexError {
+pub enum HandshakeError {
     IoError(io::Error),
     InvalidVersionExchange
 }
 
-impl fmt::Display for VexError {
+impl fmt::Display for HandshakeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            VexError::IoError(ref e) => e.fmt(f),
-            VexError::InvalidVersionExchange => write!(f, "InvalidVersionExchange")
+            HandshakeError::IoError(ref e) => e.fmt(f),
+            HandshakeError::InvalidVersionExchange => write!(f, "InvalidVersionExchange")
         }
     }
 }
 
 pub fn version_exchange<S: Io+Send+'static>(stream: BufferedIo<S>, version_string: &str, comment: &str)
-        -> Box<Future<Item=(BufferedIo<S>, Vec<u8>), Error=VexError>>
+        -> Box<Future<Item=(BufferedIo<S>, Vec<u8>), Error=HandshakeError>>
     where S: Io
 {
     let mut buf = Vec::with_capacity(256);
@@ -34,12 +34,12 @@ pub fn version_exchange<S: Io+Send+'static>(stream: BufferedIo<S>, version_strin
     }).and_then(|stream| {
         read_until(stream, b'\n', Vec::with_capacity(256))
     }).map_err(|e| {
-        VexError::IoError(e)
+        HandshakeError::IoError(e)
     }).and_then(|(stream, buf)| {
         if buf.starts_with(b"SSH-2.0-") && buf.ends_with(b"\r\n") {
             Ok((stream, buf[8 .. buf.len()-2].to_vec()))
         } else {
-            Err(VexError::InvalidVersionExchange)
+            Err(HandshakeError::InvalidVersionExchange)
         }
     }).boxed()
 }
